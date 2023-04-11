01c19b8fd3d3f4d976e71e33eaf544f742c47a38
Matthew Walker <matthewevwalker@gmail.com>
2023-04-10 21:34:04 -0500

    added the webpack plugin boilerplate code.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,7 +4,6 @@ const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
@@ -18,13 +17,31 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        title: 'Webpack Plugin Boilerplate',
+      }),      
     ],
 
     module: {
       rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'], 
+            plugins: ['@babel/plugin-proposal-object-rest-spread']
+          }
         
-      ],
+        },
+      }]
+      
     },
   };
 };
